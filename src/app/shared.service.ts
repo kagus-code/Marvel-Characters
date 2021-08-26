f250cd876e99ddf85acf6df51c2a12b700f5c7fb
Eston Kagwima <ekagwima745@gmail.com>
2021-08-26 12:41:58 +0300

    changing api url

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -19,7 +19,7 @@ PUBLIC_KEY='20e7d5df4963766c1e97bf7b3795b85c';
 HASH='eeb524518616b326afab41cd508568ffce291f8a';
 today: number = Date.now();
 
-API_URL=`https:gateway.marvel.com/v1/public/characters?events=29&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}&limit=50`;
+API_URL=`https://gateway.marvel.com/v1/public/characters?events=29&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}&limit=50`;
 
   constructor(private http:HttpClient,
     ) { }
